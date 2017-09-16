5612c5ab2e5c1e36f7e10e7b30d5ef30aa234341
d1ngoproteam <31987967+d1ngoproteam@users.noreply.github.com>
2017-09-16 14:04:38 +0300

    Update search-rnd-array-element.js

--- lw1/js/search-rnd-array-element.js
+++ lw1/js/search-rnd-array-element.js
@@ -7,16 +7,19 @@ var isFound = false;
 for (i = 0; i <= N; i++) { 
   array[i] = Math.floor(Math.random() * (16 - 1)) + 1;  
   alert("Число " + schet++ + ": " + array[i]);
-    if (array[i] == searchNumbers) {
-      isFound = true;
-	}
 }
 
 var searchNumbers = prompt("Число для поиска: "); 
 if ((searchNumbers == "") || (isNaN(searchNumbers))) {
   alert("Ошибка ввода");
   windows.stop;
-}  
+}
+
+for (i = 0; i <= N; i++) {
+  if (array[i] == searchNumbers) {
+    isFound = true;
+  }
+}  	
 
 if (!isFound) {
   alert("Число: " + searchNumbers + " не найдено");
