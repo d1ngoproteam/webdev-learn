b215e3d35432908b2f5796da763fba44b130421b
d1ngoproteam <pashkad1ngo@yandex.ru>
2017-09-16 16:53:04 +0300

    Update js
    searchNumber == null; alert

--- lw1/js/search-rnd-array-element.js
+++ lw1/js/search-rnd-array-element.js
@@ -1,8 +1,9 @@
-var counter = 1;
+﻿var counter = 1;
 var searchNumber;
 var N = 9; 
 var array = [];
-var isFound = false; 
+var isFound = false;
+var out; 
 
 for (i = 0; i <= N; i++) { 
   array[i] = Math.floor(Math.random() * (16 - 1)) + 1;  
@@ -10,7 +11,7 @@ for (i = 0; i <= N; i++) {
 }
 
 var searchNumber = prompt("Число для поиска: "); 
-if ((searchNumber == "") || (isNaN(searchNumber))) {
+if ((searchNumber == "") || (isNaN(searchNumber)) || (searchNumber == null)) {
   alert("Ошибка ввода");
   windows.stop;
 }
@@ -21,8 +22,10 @@ for (i = 0; i <= N; i++) {
   }
 }  	
 
-if (!isFound) {
-  alert("Число: " + searchNumber + " не найдено");
+if (!isFound) { 
+  out = "Число: " + searchNumber + " не найдено";
 } else {
-  alert("Число: " + searchNumber + " найдено");
+  out = "Число: " + searchNumber + " найдено";
 }
+alert(out);
+
