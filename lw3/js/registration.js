4c1a6365b0765c4d5f27291a64ca88c413571168
d1ngoproteam <31987967+d1ngoproteam@users.noreply.github.com>
2017-11-22 12:44:42 +0300

    Update js file

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -1,30 +1,30 @@
 $(document).ready(function()
 {
-    $('#registration').submit(function(event)
+  $('#registration').submit(function(event)
   {
-    event.preventDefault();
-    var email = $("#email").val();
-    var pass = $("#password").val();
-    var passCheck = $("#password_repeat").val();
-    var agreement = $("#check_agreement").prop("checked");
-    var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email));
+  event.preventDefault();
+  var email = $("#email").val();
+  var pass = $("#password").val();
+  var passCheck = $("#password_repeat").val();
+  var agreement = $("#check_agreement").prop("checked");
+  var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email));
   
-    if ((email.length < 5) || (!emailCheck)) {
-      alert("Ошибка ввода Email.");
-      return;
-    }
-    if ((pass.length < 6) || (pass == "") || (passCheck == "")) {
-      alert("Ошибка ввода пароля");
-      return;
-    }
-    if (pass != passCheck) {
-      alert("Пароли должны совпадать");
-      return;
-    }
-    if (!agreement){
-      alert("Вы должны принять пользовательское соглашение");
-      return;
-    }
-    alert("Регистрация успешна");
+  if ((email.length < 5) || (!emailCheck)) {
+    alert("Ошибка ввода Email.");
+    return;
+  }
+  if ((pass.length < 6) || (pass == "") || (passCheck == "")) {
+    alert("Ошибка ввода пароля");
+    return;
+  }
+  if (pass != passCheck) {
+    alert("Пароли должны совпадать");
+    return;
+  }
+  if (!agreement){
+    alert("Вы должны принять пользовательское соглашение");
+    return;
+  }
+  alert("Регистрация успешна");
   });
 });
