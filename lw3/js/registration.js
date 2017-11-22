2efa24bf72b3252ef40d891fd82f80071ae1cef6
d1ngoproteam <pashkad1ngo@yandex.ru>
2017-11-22 12:32:46 +0300

    Fix html and js file

--- lw3/js/registration.js
+++ lw3/js/registration.js
@@ -1,32 +1,30 @@
-$(document).ready(function(){
-    $('#registration').submit(function(event){
+$(document).ready(function()
+{
+    $('#registration').submit(function(event)
+  {
     event.preventDefault();
-    var Email = $("#email").val();
-    var Pass = $("#password").val();
-    var PassCheck = $("#password_repeat").val();
-    var Agreement = $("#check_agreement").prop("checked");
-    var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(Email));
+    var email = $("#email").val();
+    var pass = $("#password").val();
+    var passCheck = $("#password_repeat").val();
+    var agreement = $("#check_agreement").prop("checked");
+    var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email));
   
-    if ((Email.length < 5) || (!emailCheck)) {
+    if ((email.length < 5) || (!emailCheck)) {
       alert("Ошибка ввода Email.");
       return;
     }
-  
-    if ((Pass.length < 6) || (Pass == "") || (PassCheck == "")) {
+    if ((pass.length < 6) || (pass == "") || (passCheck == "")) {
       alert("Ошибка ввода пароля");
       return;
     }
-  
-    if (Pass != PassCheck) {
+    if (pass != passCheck) {
       alert("Пароли должны совпадать");
       return;
     }
-  
-    if (!Agreement){
+    if (!agreement){
       alert("Вы должны принять пользовательское соглашение");
       return;
     }
-  
     alert("Регистрация успешна");
-    });
   });
+});
